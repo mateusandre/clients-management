fd2253de34acba9ef886c7dede3e853032b40e96
Mateus <mateusandreferreira@gmail.com>
2020-09-11 12:23:46 -0400

    deploy

---
+++ precache-manifest.936db4dd7e537a86624162acac7c01c8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346883cead2c498c65f5",
+    "url": "/clients-management/css/about.3612f2ee.css"
+  },
+  {
+    "revision": "77d95d8a35350be3b6e6",
+    "url": "/clients-management/css/app.ff10d5c5.css"
+  },
+  {
+    "revision": "3224b8485e40ee81ad34",
+    "url": "/clients-management/css/chunk-vendors.c3ad6ab2.css"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/clients-management/fonts/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/clients-management/fonts/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "62c61a8d6eca45abc26e8114a35a22a6",
+    "url": "/clients-management/index.html"
+  },
+  {
+    "revision": "346883cead2c498c65f5",
+    "url": "/clients-management/js/about.e42cab2d.js"
+  },
+  {
+    "revision": "77d95d8a35350be3b6e6",
+    "url": "/clients-management/js/app.75d874e0.js"
+  },
+  {
+    "revision": "28021968769433ecc69a",
+    "url": "/clients-management/js/chunk-2d0b6942.05797341.js"
+  },
+  {
+    "revision": "4edbe059d91463fae8e0",
+    "url": "/clients-management/js/chunk-2d0c8d76.cfeb6eea.js"
+  },
+  {
+    "revision": "cd6483826cfa72437f4c",
+    "url": "/clients-management/js/chunk-2d0d6393.894b4644.js"
+  },
+  {
+    "revision": "b10f07489700e7fb9abc",
+    "url": "/clients-management/js/chunk-2d0df442.d313a0c0.js"
+  },
+  {
+    "revision": "83c2859c3f9a428adbb4",
+    "url": "/clients-management/js/chunk-2d212f97.3869bb76.js"
+  },
+  {
+    "revision": "bd8dce6f9b8dd5e15c5f",
+    "url": "/clients-management/js/chunk-7712de25.a8dc1718.js"
+  },
+  {
+    "revision": "3224b8485e40ee81ad34",
+    "url": "/clients-management/js/chunk-vendors.c91507c7.js"
+  },
+  {
+    "revision": "e1c2c1e216139ee1d0d10d1e792711e3",
+    "url": "/clients-management/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/clients-management/robots.txt"
+  }
+]);
